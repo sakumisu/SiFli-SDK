sakumisu/SiFli-SDK

commit 9aee2faebd578246d24c1c4e3aca831a97f5e899

[bug][drv] Fix sf32lb52 letter series ADC measurement abnormal as chip id is not detected correctly
ext-redmine: #6162, REDMINE-6162

[Description in detail]

Change-Id: I3bcecd15ecc13a8b723b61cefbdc5065ab89b6c5

drivers/Include/bf0_hal.h
@@ -110,11 +110,19 @@ extern "C" {
 #define __HAL_SYSCFG_GET_SID()            (hwp_hpsys_cfg->IDR>>HPSYS_CFG_IDR_SID_Pos)           /*!< Get serial ID*/
 #define __HAL_SYSCFG_GET_CID()            ((hwp_hpsys_cfg->IDR>>HPSYS_CFG_IDR_CID_Pos)&0xff)    /*!< Get Chip ID*/
 #define __HAL_SYSCFG_GET_PID()            ((hwp_hpsys_cfg->IDR>>HPSYS_CFG_IDR_PID_Pos)&0xff)    /*!< Get Package ID*/
-#ifndef SOC_SF32LB55X
+#ifndef SF32LB55X
 #define __HAL_SYSCFG_GET_REVID()          (hwp_hpsys_cfg->IDR&0xff)                             /*!< Get Revision ID*/
 #else
 #define __HAL_SYSCFG_GET_REVID()          ((hwp_hpsys_cfg->USBCR>>HPSYS_CFG_USBCR_RSVD1_Pos)&0xff) /*!< Get Revision ID*/
-#endif
+#endif /* SF32LB55X */
+
+#ifdef SF32LB52X
+#define __HAL_SYSCFG_CHECK_REVID()      HAL_ASSERT((__HAL_SYSCFG_GET_REVID() == HAL_CHIP_REV_ID_A3) || \
+                                                   (__HAL_SYSCFG_GET_REVID() == HAL_CHIP_REV_ID_A4) || \
+                                                   (__HAL_SYSCFG_GET_REVID() == HAL_CHIP_REV_ID_B4))
+#else
+#define __HAL_SYSCFG_CHECK_REVID()
+#endif /* SF32LB52X */
 
 #define __HAL_SYSCFG_Enable_USB()         (hwp_hpsys_cfg->USBCR|=HPSYS_CFG_USBCR_USB_EN)        /*!< Enable USB module*/
 #define __HAL_SYSCFG_USB_DM_PD()         (hwp_hpsys_cfg->USBCR|=HPSYS_CFG_USBCR_DM_PD)        /*!< Pull Down USB DM pin, host only*/
@@ -203,17 +211,17 @@ extern "C" {
 
 /** @brief  CHIP ID for A3.
   */
-#ifdef SOC_SF32LB55X
+#ifdef SF32LB55X
 #define HAL_CHIP_REV_ID_A3 0x80
+#elif defined(SF32LB52X)
+#define HAL_CHIP_REV_ID_A3 0x03
+/* actually it's the B3 revision id */
+#define HAL_CHIP_REV_ID_A4 0x07
+#define HAL_CHIP_REV_ID_B4 0x0F
 #else
 #define HAL_CHIP_REV_ID_A3 0xFF // Not defined
-#endif
-
-#ifdef SF32LB52X
-#define HAL_CHIP_REV_ID_A4 0x07
-#else
 #define HAL_CHIP_REV_ID_A4 0xFF
-#endif
+#endif /* SF32LB55X */
 
 
 #ifdef SF32LB52X

drivers/cmsis/sf32lb52x/register.h
@@ -711,7 +711,7 @@ typedef enum
 
 #define IS_LCPU(id)  ((*id)&1)
 
-#define SF32LB52X_LETTER_SERIES()  (__HAL_SYSCFG_GET_REVID() == HAL_CHIP_REV_ID_A4)
+#define SF32LB52X_LETTER_SERIES()  ((__HAL_SYSCFG_GET_REVID() == HAL_CHIP_REV_ID_A4) || (__HAL_SYSCFG_GET_REVID() == HAL_CHIP_REV_ID_B4))
 
 #if defined (USE_HAL_DRIVER)
 #include "bf0_hal.h"

rtos/rtthread/bsp/sifli/drivers/drv_common.c
@@ -669,6 +669,7 @@ RT_WEAK void rt_hw_board_init()
 #ifdef SOC_BF0_HCPU
     rt_kprintf("Serial:%x,Chip:%x,Package:%x,Rev:%x  Reason:%08x\r\n",
                __HAL_SYSCFG_GET_SID(), __HAL_SYSCFG_GET_CID(), __HAL_SYSCFG_GET_PID(), __HAL_SYSCFG_GET_REVID(), HAL_PMU_GET_WSR());
+    __HAL_SYSCFG_CHECK_REVID();
 
 #ifdef RT_USING_PM
     rt_kprintf("Serial PowerOnMOde:%d rtc_record:%08x\n", SystemPowerOnModeGet(), HAL_Get_backup(RTC_BACKUP_MODULE_RECORD));

commit 167ee53fe0010126882ae71f859ca499ee9291e0

[bug][drv] Fix sf32lb52 rev id check error introduced by 9aee2faeb
ext-redmine: #6162, REDMINE-6162

[Description in detail]

Change-Id: Ie2e38a5f9c133539fd1f7f1aaaa0ad157ac83dee

drivers/Include/bf0_hal.h
@@ -117,7 +117,7 @@ extern "C" {
 #endif /* SF32LB55X */
 
 #ifdef SF32LB52X
-#define __HAL_SYSCFG_CHECK_REVID()      HAL_ASSERT((__HAL_SYSCFG_GET_REVID() == HAL_CHIP_REV_ID_A3) || \
+#define __HAL_SYSCFG_CHECK_REVID()      HAL_ASSERT((__HAL_SYSCFG_GET_REVID() <= HAL_CHIP_REV_ID_A3) || \
                                                    (__HAL_SYSCFG_GET_REVID() == HAL_CHIP_REV_ID_A4) || \
                                                    (__HAL_SYSCFG_GET_REVID() == HAL_CHIP_REV_ID_B4))
 #else